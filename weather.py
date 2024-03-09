761cc7afaacfd72b520805ca87d1d84f4631e760
digimori <hanaorikuu@gmail.com>
2024-03-09 14:32:41 +0000

    Added search box for user input - will likely make this larger later

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 # Fetching the app call:
 root = Tk()
@@ -7,7 +8,13 @@ root = Tk()
 root.title("Weather App")
 root.geometry('800x650')
 
+# Allow user to input the city that they want to check the weather for:
+# This generates a text box for user input
+input_city = StringVar()
 
+# Taking the input and the root defined above as parameters:
+city_calc = Entry(root, textvariable=input_city)
+city_calc.pack()
 
 # This must be kept at the bottom to stop the GUI destroying itself every reset
 root.mainloop()
